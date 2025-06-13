98b7790cf361535dfdb72c7e299f8c4ec2b6d3e4
R3CI <extiverooo@proton.me>
2025-06-13 09:47:55 +0200

    error db

--- src/util/logger.py
+++ src/util/logger.py
@@ -27,3 +27,41 @@ class logger:
             ts = ''
 
         print(f'{ts}{co.main}[{co.reset}{self.module}{co.main}] {co.main}[{co.red}{text}{co.main}] {endstr}{co.reset}')
+
+    def errordatabase(self, text):
+        db = {
+            '10014': 'Unknown emoji',
+            '30010': 'Max reactions on message',
+            '40007': 'Banned token',
+            '40002': 'Locked token',
+            '50109': 'Invalid JSON',
+            '200000': 'Automod flagged',
+            '50007': 'Action not allowed',
+            '50008': 'Unable to send',
+            '50001': 'No access/Not inside',
+            '50013': 'Missing permissions',
+            '50024': 'Cant do that on this channel',
+            '80003': 'Cant self friend',
+            '50168': 'Not in a VC',
+            '20028': 'Limited',
+            '340013': 'Muted/Acces to send messages limited by server',
+            '401: Unauthorized': 'Invalid token/Lock invalided token',
+            'Cloudflare': 'Cloudflare',
+            'captcha_key': 'Hcaptcha',
+            'Unauthorized': 'Invalid token/Lock invalided token',
+            'retry_after': 'Limited',
+            'You need to verify': 'Locked token',
+            'Cannot send messages to this user': 'Disabled DMS',
+            'You are being blocked from accessing our API': 'API BAN',
+            'Unknown Invite': 'Unknown Invite',
+            '150009': 'Alerdy a member (no need to verify)',
+            '50055': 'Invalid server',
+            '50009': 'Verification too high (server requires PV, EV or being in server for 10 mins)',
+            '50035': 'Invalid JSON'
+        }
+
+        for key in db.keys():
+            if key in text:
+                return db[key]
+
+        return text
