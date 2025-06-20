9320042c8e0d0fc292224c55bc127dc6e018e26d
R3CI <extiverooo@proton.me>
2025-06-20 19:35:31 +0200

    fix

--- src/util/autoupdate.py
+++ src/util/autoupdate.py
@@ -1,10 +1,8 @@
-import os
-import sys
-import requests
-import zipfile
-import shutil
-import tempfile
-from pathlib import Path
+# This code is the property of R3CI.
+# Unauthorized copying, distribution, or use is prohibited.
+# Licensed under the GNU General Public License v3.0 (GPL-3.0).
+# For more details, visit https://github.com/R3CI/G4Spam
+
 from src import *
 from src.util.logger import logger
 from src.util.ui import ui
